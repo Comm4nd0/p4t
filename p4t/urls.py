06dec5557d2b97069e28b47538d93c231cf792e4
Marco Baldanza <marco@X303>
2018-09-13 22:45:58 +0100

    added about and the team

--- p4t/urls.py
+++ p4t/urls.py
@@ -5,7 +5,9 @@ from django.conf.urls.static import static
 from paws4thought import views
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', views.home, name='home'),
-    path('services/', include('services.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+                  path('admin/', admin.site.urls),
+                  path('', views.home, name='home'),
+                  path('about/', views.about, name='about'),
+                  path('team/', views.team, name='team'),
+                  path('services/', include('services.urls'))
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- paws4thought/views.py
+++ paws4thought/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(requests):
     return render(requests, 'home.html')
+
+def about(requests):
+    return render(requests, 'about.html')
+
+def team(requests):
+    return render(requests, 'team.html')
